c5115d449b633918bd6aba7d2d8129f25792c2a7
Joel Santos <joeltere9@gmail.com>
2025-03-27 16:41:22 -0300

    first commit

---
+++ src/test/java/com/joel/br/Nba/Stats/NbaStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.Nba.Stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
